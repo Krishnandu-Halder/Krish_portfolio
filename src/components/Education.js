e4532d2a0e78acd07e2340c6bf2ae840b293216f
Krish <krishnenduhalder313@gmail.com>
2025-05-23 20:33:41 +0530

    Updated the portfolio

--- src/components/Education.js
+++ src/components/Education.js
@@ -49,7 +49,7 @@ const Education = () => {
       '/>
         <ul className='w-full flex flex-col items-start justify-between  ml-4 xs:ml-7'>
             <Details
-            type='Bachelor Of Science In Computer Science' time="2016-2020"
+            type='Bachelor Of Science In Computer Science' time="2022-2026"
             place="https://www.brainwareuniversity.ac.in/ Brainware University"
             info= "Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial Intelligence." />
             <Details
